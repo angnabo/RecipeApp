fab852ba808a0c5a053ba9ad22b6763d073fa383
arovar <angnabo@github.com>
2020-03-29 14:45:38 +0100

    upated gitignore

--- recipeApp/config.py
+++ recipeApp/config.py
@@ -2,3 +2,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '504664732235-92vj5ptflf1aap4aq9f16j2mj8kojfd7.a
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'Ir4OLrziwi8dumjewjNy5373'
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/recipes/'
 SOCIAL_AUTH_LOGIN_URL = '/'
+
+#emails
+EMAIL_HOST_USER = 'angelica.nabozhina@gmail.com'
+EMAIL_HOST_PASSWORD = 'Crystallized1'
