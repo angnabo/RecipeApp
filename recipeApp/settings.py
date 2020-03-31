dc0caee72d6ff6a883ce2b2ea01697018b2b7640
arovar <angnabo@github.com>
2020-03-31 16:52:55 +0100

    fixing migrations

--- recipeApp/settings.py
+++ recipeApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gqosx07_w1q029pv3x=031%@e6qo%po8@a!q8#z^%##u+(bdm2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
@@ -96,12 +96,11 @@ SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'recipeapp',
-        'USER': 'recipeappuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
